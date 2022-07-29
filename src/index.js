5fa75780879c9fdb03439b2c303600488f89e527
mohsin <mohsinbwp2018@gmail.com>
2022-07-29 18:52:57 +0500

    ..

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-
-  var slideIndex = 1;
-  showDivs(slideIndex);
-
-  function plusDivs(n) {
-    showDivs(slideIndex += n);
-  }
-
-
-  function showDivs(n) {
-    
-    var x = document.getElementsByClassName("mySlides");
-    var t = document.getElementsByClassName("slider-text");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length }
-    
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    
  
+
+  let i=0;
+
+  function move(i)
+  {
+    if(i<1)
+    {
+      document.getElementById("slider").style.backgroundImage = "url(https://daks2k3a4ib2z.cloudfront.net/5527a015715879bc5767ece2/55299e51264213136f7d6a43_hoop.jpg)";
     }
-    
-    x[slideIndex - 1].style.display = "block";
-        
+    if(i>3)
+    {
+      document.getElementById("slider").style.backgroundImage = "url(https://daks2k3a4ib2z.cloudfront.net/5527a015715879bc5767ece2/55299d1b0108cf394df61d5a_sheep.jpeg)";
   }
+if(i=2)
+{
+  document.getElementById("slider").style.backgroundImage = "url(https://daks2k3a4ib2z.cloudfront.net/5527a015715879bc5767ece2/5527a395e02754bb57746389_slider1.jpg)";
+  
+}
+}
+
